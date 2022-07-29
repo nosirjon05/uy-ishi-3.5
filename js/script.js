b4ad8d9c5e6680433837805bbef9c0aefd108128
nosirjon05 <nosirjonkomilov05@gmail.com>
2022-07-29 15:37:22 +0500

    Add code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
+alert("xush kelibsiz")
 var first_name = prompt("ismingiz nma?");
 var last_name = prompt("familyangiz nma?");
 var young = prompt("yoshingiz nechida?");
 var minyoung = 16
+alert("malumotlar uchun rahmat")
 
 if (young >= minyoung) {
   alert(first_name + " Marhamat kiring");
